ace2bfbffe06e9be45bba68117472513b4634fc9
julia <jerlo180908@gmail.com>
2026-03-09 11:04:12 -0300

    realiza ajustes para rodar CRUD

--- src/routes/lanchoneteRoute.js
+++ src/routes/lanchoneteRoute.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import autenticarApikey from './../utils/apiKey.js'
 import * as controller from '../controllers/clientController.js';
 import * as controller1 from '../controllers/pedidoController.js';
 import * as controller2 from '../controllers/produtoControllers.js';
