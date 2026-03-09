b204105ba44cf10da0b5a948aad31cc38dc94de5
Lívia <lligiaoliveira00@gmail.com>
2026-03-09 11:44:30 -0300

    consegui fazer o clima

--- src/routes/lanchoneteRoute.js
+++ src/routes/lanchoneteRoute.js
@@ -4,7 +4,7 @@ import * as controller from '../controllers/clientController.js';
 import * as controller1 from '../controllers/pedidoController.js';
 import * as controller2 from '../controllers/produtoControllers.js';
 import * as controller3 from '../controllers/itemPedidoController.js';
-import autenticar from '../utils/apiKey.js';
+
 
 const router = express.Router();
 
